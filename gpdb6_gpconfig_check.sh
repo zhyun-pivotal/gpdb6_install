fd4acef19488850d0d5020a639c26cfaeb0000ac
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2022-07-25 10:49:55 +0900

    Update gpdb6_gpconfig_check.sh

--- gpdb6_gpconfig_check.sh
+++ gpdb6_gpconfig_check.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # gp_vmem_protect_limit needed calculate refer pysical memory
 gpconfig -s gp_vmem_protect_limit
-gpconfig -s gp_autostat_mode
-gpconfig -s tcp_keepalives_count
-gpconfig -s tcp_keepalives_idle
-gpconfig -s tcp_keepalives_interval
 gpconfig -s xid_warn_limit
 gpconfig -s xid_stop_limit
 gpconfig -s gp_external_enable_exec
@@ -19,4 +15,9 @@ gpconfig -s max_connections
 gpconfig -s max_prepared_transactions
 gpconfig -s superuser_reserved_connections
 gpconfig -s gp_enable_gpperfmon
-gpconfig -s optimizer
+gpconfig -s gp_workfile_compression
+#gpconfig -s optimizer
+#gpconfig -s gp_autostat_mode
+#gpconfig -s tcp_keepalives_count
+#gpconfig -s tcp_keepalives_idle
+#gpconfig -s tcp_keepalives_interval
