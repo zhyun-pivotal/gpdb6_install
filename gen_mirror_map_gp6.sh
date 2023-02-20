f6df8c405e3758439a9d1f511d60fc19af4e894a
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-02-20 22:25:18 +0900

    Update gen_mirror_map_gp6.sh

--- gen_mirror_map_gp6.sh
+++ gen_mirror_map_gp6.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-## explain
-## MAXCONTENT : Number of primary instances in four segments (start 0, 16 = seg_nodes 4 * instances per seg_nodes 4)
-## SEGPREFIX  : segment node prefix name setting
-## INSTPREFIX : segment instance prefix name setting
-## SNUM/ENUM  : Number of quater groups to add (quater = 4 seg_nodes, start 0, if 2nd fullrack = (4,7)
+echo "=========================================================================================="
+echo "CAUTION!"
+echo "1. Segment node hostname have to be composed of work type (prefix) + sequence number."
+echo "2. Segment nodes have to be expanded in units of 4 units."
+echo "3. Do not put empty values in all input fields."
+echo "=========================================================================================="
 
 ## check user env
 MAXCONTENT=`psql -Atc "select max(content)+1 from gp_segment_configuration;"`
 SEGHOSTNM=`psql -Atc "select hostname from gp_segment_configuration where content=0 and preferred_role='p';"`
 echo "Segment node hostname is configed prefix_name + number like this : " ${SEGHOSTNM}
-read -p "What is the segment node's prefix_name in your segment node? : " SEGPREFIX
-read -p "How many zeros are in the segment node hostname? [0/1/2] : " ZEROCNT
+read -p "(Input1)What is the segment node's prefix_name in your segment node? : " SEGPREFIX
+read -p "(Input2)How many zeros are in the segment node hostname? [0/1/2] : " ZEROCNT
 INSTNM=`psql -Atc "select datadir from gp_segment_configuration where content=0 and preferred_role='p';" | awk -F'/' '{print $4}'`
 echo "Segment instance name is configed prefix_name + contnet like this : " ${INSTNM}
-read -p "What is the instance's prefix_name in your cluster? : " INSTPREFIX
-read -p "What is segment node number of the first expand nodes? : " SCNT
-read -p "What is segment node number of the last expand nodes? : " ECNT
+read -p "(Input3)What is the instance's prefix_name in your cluster? : " INSTPREFIX
+read -p "(Input4)What is segment node number of the first expand nodes? : " SCNT
+read -p "(Input5)What is segment node number of the last expand nodes? : " ECNT
 SNUM=`expr ${SCNT} / 4`
 ENUM=`expr \( ${ECNT} - 1 \) / 4`
 
@@ -91,6 +92,6 @@ FROM
 " > ./gen_mirror_map.out
 
 echo ""
-echo "=================================================="
+echo "=========================================================================================="
 echo "Succes : result write in the ./gen_mirror_map.out file"
-echo "=================================================="
+echo "=========================================================================================="
