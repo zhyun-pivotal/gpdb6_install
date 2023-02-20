fee8ac302e1a3452870b4858699b6ad4ba962816
zhyun-pivotal <52663478+zhyun-pivotal@users.noreply.github.com>
2023-02-20 22:50:51 +0900

    Update and rename gen_mirror_map_gp6.sh to crt_expand_map_gp6.sh

--- crt_expand_map_gp6.sh
+++ crt_expand_map_gp6.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 echo "=========================================================================================="
-echo "REQUIREMENT"
+echo "REQUIREMENT!"
 echo "1. Segment node hostname have to be composed of work type (prefix) + sequence number."
-echo "2. Segment nodes have to be expanded in units of 4 units."
+echo "2. Segment node have to be expanded by 4 units."
 echo "3. Do not put empty values in all input fields."
 echo "=========================================================================================="
+echo ""
 
 ## check user env
 MAXCONTENT=`psql -Atc "select max(content)+1 from gp_segment_configuration;"`
 SEGHOSTNM=`psql -Atc "select hostname from gp_segment_configuration where content=0 and preferred_role='p';"`
-echo "Segment node hostname is configed prefix_name + number like this : " ${SEGHOSTNM}
-read -p "(Input1)What is the segment node's prefix_name in your segment node? : " SEGPREFIX
-read -p "(Input2)How many zeros are in the segment node hostname? [0/1/2] : " ZEROCNT
+echo "   Segment node hostname is configed prefix_name + number like this  >> " ${SEGHOSTNM} " <<"
+read -p "1. What is the segment node's prefix_name in your segment node? : " SEGPREFIX
+read -p "2. How many zeros are in the segment node hostname? [ 0 | 1 | 2 ] : " ZEROCNT
 INSTNM=`psql -Atc "select datadir from gp_segment_configuration where content=0 and preferred_role='p';" | awk -F'/' '{print $4}'`
-echo "Segment instance name is configed prefix_name + contnet like this : " ${INSTNM}
-read -p "(Input3)What is the instance's prefix_name in your cluster? : " INSTPREFIX
-read -p "(Input4)What is segment node number of the first expand nodes? : " SCNT
-read -p "(Input5)What is segment node number of the last expand nodes? : " ECNT
+echo "   Segment instance name is configed prefix_name + contnet like this  >> " ${INSTNM} " <<"
+read -p "3. What is the instance's prefix_name in your cluster? : " INSTPREFIX
+read -p "4. What is segment node number of the first expand nodes? : " SCNT
+read -p "5. What is segment node number of the last expand nodes? : " ECNT
 SNUM=`expr ${SCNT} / 4`
 ENUM=`expr \( ${ECNT} - 1 \) / 4`
 
@@ -89,9 +90,9 @@ FROM
   FROM   template_            AS tm,
          generate_series(${SNUM},${ENUM}) AS i
   ORDER BY 2, 1) AS lt ;
-" > ./gen_mirror_map.out
+" > ./crt_expand_map.out
 
 echo ""
 echo "=========================================================================================="
-echo "Succes : result write in the ./gen_mirror_map.out file"
+echo "Succes : result write in the ./crt_expand_map.out file"
 echo "=========================================================================================="
